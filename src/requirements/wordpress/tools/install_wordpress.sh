d2b89e363cdd784556a81921f49a569d34c30922
Ayman Bouabra <abouabra@c2r4p1.1337.ma>
2023-06-04 19:00:52 +0100

    done with nginx container

---
+++ src/requirements/wordpress/tools/install_wordpress.sh
@@ -0,0 +1,9 @@
+rm -rf *
+wget http://wordpress.org/latest.tar.gz
+tar -xzf /var/www/html/latest.tar.gz
+mv wordpress/* .
+rm -rf latest.tar.gz wordpress
+cp wp-config-sample.php wp-config.php
+
+sed -i 's/;extension=curl/extension=mysqli/' /etc/php/7.4/cgi/php.ini
+sed -i 's/listen = /var/run/php5-fpm.sock/listen = 0.0.0.0:9000/' /etc/php/7.4/fpm/pool.d/www.conf
