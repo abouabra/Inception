8a0c54b725a7be890f1cf84efd95c15e8607bff1
Ayman Bouabra <abouabra@c2r4p1.1337.ma>
2023-06-04 19:22:01 +0100

    working on wordpress

--- src/requirements/wordpress/tools/install_wordpress.sh
+++ src/requirements/wordpress/tools/install_wordpress.sh
@@ -7,4 +7,4 @@ rm -rf latest.tar.gz wordpress
 cp wp-config-sample.php wp-config.php
 
 sed -i 's/;extension=curl/extension=mysqli/' /etc/php/7.4/cgi/php.ini
-sed -i 's/listen = /var/run/php5-fpm.sock/listen = 0.0.0.0:9000/' /etc/php/7.4/fpm/pool.d/www.conf
+sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 0.0.0.0:9000/' /etc/php/7.4/fpm/pool.d/www.conf
