73d612fe16e41797ca442592b8e5fa08cc7ae961
Ayman Bouabra <abouabra@c2r4p1.1337.ma>
2023-06-06 00:22:41 +0100

    started working on bonus

--- src/requirements/wordpress/tools/install_wordpress.sh
+++ src/requirements/wordpress/tools/install_wordpress.sh
@@ -18,4 +18,6 @@ sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 0.0.0.0:9000/' /etc/php/
 
 wp core install --url="${WORDPRESS_URL}" --title="${WORDPRESS_TITLE}" --admin_user=${WORDPRESS_ADMIN_USER} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_EMAIL} --allow-root
 
+wp theme activate twentytwentyone  --allow-root
+
 /usr/sbin/php-fpm7.4 -F 
