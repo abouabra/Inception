2f82dda816c3e1a676791cc222551faf741a5e77
Ayman Bouabra <abouabra@c2r4p1.1337.ma>
2023-06-05 16:18:06 +0100

    working on mariadb

--- src/requirements/wordpress/tools/install_wordpress.sh
+++ src/requirements/wordpress/tools/install_wordpress.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
 cd /var/www/html/
-wget http://wordpress.org/latest.tar.gz
-tar -xzf /var/www/html/latest.tar.gz
-mv wordpress/* .
-rm -rf latest.tar.gz wordpress
-cp wp-config-sample.php wp-config.php
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
 
-sed -i 's/;extension=curl/extension=mysqli/' /etc/php/7.4/cgi/php.ini
-sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 0.0.0.0:9000/' /etc/php/7.4/fpm/pool.d/www.conf
+wp core download --path=/var/www/html --locale=en_US --allow-root
+
+cp wp-config-sample.php wp-config.php
 sed -i 's/database_name_here/wordpress/' /var/www/html/wp-config.php
 sed -i 's/username_here/abouabra/' /var/www/html/wp-config.php
 sed -i 's/password_here/1598753246/' /var/www/html/wp-config.php
 sed -i 's/localhost/mariadb/' /var/www/html/wp-config.php
+
+sed -i 's/;extension=curl/extension=mysqli/' /etc/php/7.4/cgi/php.ini
+sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 0.0.0.0:9000/' /etc/php/7.4/fpm/pool.d/www.conf
 
 /usr/sbin/php-fpm7.4 -F
