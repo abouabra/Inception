afc015f7a7dc2b132865eefcb5828f41adf8e158
Ayman Bouabra <abouabra@c2r4p1.1337.ma>
2023-06-05 22:16:49 +0100

    working on compose

--- src/requirements/mariadb/tools/install_mariadb.sh
+++ src/requirements/mariadb/tools/install_mariadb.sh
@@ -2,16 +2,16 @@
 
 service mariadb start 
 
-mysql_secure_installation <<EOF
+# mysql_secure_installation <<EOF
 
-y
-1598753246
-1598753246
-y
-y
-y
-y
-EOF
+# y
+# 1598753246
+# 1598753246
+# y
+# y
+# y
+# y
+# EOF
 
 mariadb -e "CREATE DATABASE wordpress;"
 mariadb -e "CREATE USER 'abouabra'@'%' IDENTIFIED BY '1598753246' ;"
@@ -19,6 +19,7 @@ mariadb -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'abouabra'@'%' ;"
 mariadb -e "FLUSH PRIVILEGES;"
 mariadb -e "SHOW DATABASES;"
 
-kill $(cat /var/run/mysqld/mysqld.pid)
+# kill $(cat /var/run/mysqld/mysqld.pid)
+service mariadb stop
 
 mysqld
