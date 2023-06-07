575e0adab650d7a490a38d0796f136bd27b76bd3
Ayman Bouabra <abouabra@c2r4p1.1337.ma>
2023-06-07 18:02:41 +0100

    started working on bonus

--- src/requirements/bonus/ftp/conf/configure_ftp.sh
+++ src/requirements/bonus/ftp/conf/configure_ftp.sh
@@ -9,9 +9,10 @@ mkdir /var/ftp_home
 
 useradd ${FTP_USER}
 
-echo -e "${FTP_PASSWORD}\n${FTP_PASSWORD}" | passwd ${FTP_USER}
 chown ${FTP_USER}:${FTP_USER} /var/ftp_home
 
 usermod -d /var/ftp_home/ ${FTP_USER}
+
+echo -e "${FTP_PASSWORD}\n${FTP_PASSWORD}" | passwd ${FTP_USER}
 
 sleep 9999999999
