35c45de8f0ab3afcad81a1737c6f5419ef507f36
Ayman Bouabra <abouabra@c2r4p1.1337.ma>
2023-06-07 16:38:30 +0100

    started working on bonus

--- src/requirements/bonus/ftp/conf/configure_ftp.sh
+++ src/requirements/bonus/ftp/conf/configure_ftp.sh
@@ -5,4 +5,12 @@ service vsftpd start
 
 sed -i "s/#write_enable=YES/write_enable=YES/" /etc/vsftpd.conf
 
+mkdir /var/ftp_home
+
+useradd -p ${FTP_PASSWORD} ${FTP_USER}
+
+chown ${FTP_USER}:${FTP_USER} /var/ftp_home
+
+usermod -d /var/ftp_home/ ${FTP_USER}
+
 sleep 9999999999
