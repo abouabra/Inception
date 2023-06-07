23f36b4e08dd7b269125d8c8458b6068474f0a6e
Ayman Bouabra <abouabra@c2r4p1.1337.ma>
2023-06-07 19:22:51 +0100

    started working on bonus

--- src/requirements/bonus/ftp/conf/configure_ftp.sh
+++ src/requirements/bonus/ftp/conf/configure_ftp.sh
@@ -8,11 +8,11 @@ sed -i "s/#write_enable=YES/write_enable=YES/" /etc/vsftpd.conf
 mkdir /var/ftp_home
 
 useradd ${FTP_USER}
+echo "${FTP_USER}:${FTP_PASSWORD}" | chpasswd
 
 chown ${FTP_USER}:${FTP_USER} /var/ftp_home
 
 usermod -d /var/ftp_home/ ${FTP_USER}
 
-echo -e "${FTP_PASSWORD}\n${FTP_PASSWORD}" | passwd ${FTP_USER}
 
 sleep 9999999999
