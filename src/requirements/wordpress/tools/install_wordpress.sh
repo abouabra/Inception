875582d9a83c65efd763471007ecc962d4f291a9
Ayman Bouabra <abouabra@c2r4p1.1337.ma>
2023-06-07 19:44:32 +0100

    started working on bonus

--- src/requirements/wordpress/tools/install_wordpress.sh
+++ src/requirements/wordpress/tools/install_wordpress.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-cd /var/www/html/
+# cd /var/www/html/
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
