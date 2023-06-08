8271b04da02ad87a91e1c261cd8de142cfc574c6
Ayman Bouabra <abouabra@c2r4p1.1337.ma>
2023-06-08 14:01:14 +0100

    started working on bonus

--- src/requirements/bonus/redis/conf/configure_redis.sh
+++ src/requirements/bonus/redis/conf/configure_redis.sh
@@ -1,7 +1,7 @@
 !/bin/sh
 
-# sed -i "s/#bind 127.0.0.1/bind 127.0.0.1/"  /etc/redis/redis.conf
+sed -i "s/#bind 127.0.0.1/bind 127.0.0.1/"  /etc/redis/redis.conf
 
-# redis-server --protected-mode no
+redis-server --protected-mode no
 
-sleep 99999999999
+# sleep 99999999999
